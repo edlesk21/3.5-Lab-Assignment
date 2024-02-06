fcc0bffbca36e0f83f407d5be52a177c84f42e7d
EJ Leskauskas <leskaus1@msu.edu>
2024-02-05 19:28:34 -0500

    push changes

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,4 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
-
-  eleventyConfig.addShortcode("user", function(name, Useremail){
-    return <div class = "user">
-<div class = "user_name">${name}</div>
-<div class = "user_email">@${Useremail}</div>
-</div>;});
 };
