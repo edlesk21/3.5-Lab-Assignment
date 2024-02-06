ca5497a3c10d69b13cdcad6a18cc6cb255e6847b
EJ Leskauskas <leskaus1@msu.edu>
2024-02-05 19:16:06 -0500

    Submit

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  eleventyConfig.addShortcode("user", function(name, Useremail){
+    return '<div class = "user"><div class = "user_name">${name}</div><div class = "user_email">@${Useremail}</div></div>';});
 };
